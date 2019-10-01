02749971a284e04f5d33b5fb64e845442a57ecc9
thanaponyalan <thanaponyalan@gmail.com>
2019-10-01 15:05:45 +0700

    Lab06

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
+from .models import List
+from .forms import ListForm
 
 #Create your views here.
 def index(request):
-    today=datetime.datetime.now().date()
-    return render(request,'index.html',{"today" : today})
-    #return HttpResponse("Hello World!, you are at the todoApp index page.")
+    if request.method=='POST':
+        form=ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            allItems=List.objects.all
+            return render(request,'index.html',{'all':allItems})
+        
+    else:
+        allItems=List.objects.all
+        return render(request,'index.html',{'all': allItems})
 
 def about(request):
     return render(request,'about.html')
-#    return HttpResponse("This is the about page of todoApp.")
 
 def contact(request):
     name="Thanapon"
